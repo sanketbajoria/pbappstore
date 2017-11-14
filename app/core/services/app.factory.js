dc5c7aa9a00692d8e4b83b1fe43deb134c89d47f
Sanket Bajoria <bajoriasanket@gmail.com>
2017-11-14 17:43:34 +0530

    icon middle

--- app/core/services/app.factory.js
+++ app/core/services/app.factory.js
@@ -11,6 +11,18 @@
         }
       }).then(function (r) {
         return r.data.categories.filter(function (c) {
+          if (c.id === 168) {
+            c.description = 'Send, Receive, Track, Sort, Go Global';
+          }
+          if (c.id === 169) {
+            c.description = 'International expansion and global retail made easy';
+          }
+          if (c.id === 170) {
+            c.description = 'Reach out to new customers, analyse competition, build digital presence';
+          }
+          if (c.id === 171) {
+            c.description = 'Listen to your customers, manage tickets, resolve quickly';
+          }
           return !c.is_stock;
         });
       }),
